be671c6d48964cfc22d828fd2625a1919df5de52
SylvainDuret <sylduret@hotmail.fr>
2021-02-16 14:03:24 +0100

    coucou hugues

--- scripts/2048.js
+++ scripts/2048.js
@@ -6,7 +6,7 @@ var config = {
       create: create,    // Initialisation des variables & objets
       update: update     // Fonction appelée 60 fois par seconde
     },
-    parent: '2048', // Affiche le jeu dans le div id="flappyBird"
+    parent: '2048', // Affiche le jeu dans le div id="2048"
     physics: {
       default: 'arcade', // Permet d'appliquer un set de mouvements aux objets
       arcade: {
